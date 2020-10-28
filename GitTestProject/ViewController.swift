dde3ec98050d83083040477868fc149c85fa0640
Andrei Ramescu <>
2020-10-28 11:35:52 +0100

    Test For Git

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Andrei Ramescu on 28/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
